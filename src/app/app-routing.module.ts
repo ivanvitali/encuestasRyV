f5a735b47c514b99e0cb8ac50f83b3766710ccfb
Ivan Vitali <webcoder.ivan@gmail.com>
2018-08-25 18:56:54 -0300

    trabajando con el componente estadistica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { WelcomeComponent } from "./welcome/welcome.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { LoginComponent } from "./auth/login/login.component";
 import { SurveyComponent } from "./survey/survey.component";
+import { StatisticsComponent } from "./statistics/statistics.component";
+import { VihStatisticsComponent } from "./statistics/vih-statistics/vih-statistics.component";
 
 const routes: Routes = [
     { path: '', component: WelcomeComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'survey', component: SurveyComponent }
+    { path: 'survey', component: SurveyComponent },
+    { path: 'statistics', component: StatisticsComponent }
 ];
 
 @NgModule({
