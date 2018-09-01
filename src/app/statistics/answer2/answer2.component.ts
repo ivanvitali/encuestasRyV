33a5dafb5a0b94621ba0a18c8dbf89c4cb037ef7
Ivan Vitali <webcoder.ivan@gmail.com>
2018-08-31 23:25:40 -0300

    se trabajo answer2.service y answer2.component, se puede ver la estadistica de la pregunta 2

--- src/app/statistics/answer2/answer2.component.ts
+++ src/app/statistics/answer2/answer2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Answer2Service } from './answer2.service';
 
@@ -7,14 +7,68 @@ import { Answer2Service } from './answer2.service';
   templateUrl: './answer2.component.html',
   styleUrls: ['./answer2.component.css']
 })
-export class Answer2Component implements OnInit {
+export class Answer2Component implements OnInit, OnDestroy {
 
   answer2Man1517StatisticsSubscription: Subscription;
+  answer2Man1821StatisticsSubscription: Subscription;
+  answer2Man2230StatisticsSubscription: Subscription;
+  answer2Woman1517StatisticsSubscription: Subscription;
+  answer2Woman1821StatisticsSubscription: Subscription;
+  answer2Woman2230StatisticsSubscription: Subscription;
+
+  private statistics = {
+    answer2 : {
+      man1517 : {
+        title: "Vih == Sida?",
+        subTitle: "Hombre 15-17",
+        tagId: "chart-man-15-17-answer2",
+        data: [{}]
+      },
+      man1821 : {
+        title: "Vih == Sida?",
+        subTitle: "Hombre 18-21",
+        tagId: "chart-man-18-21-answer2",
+        data: [{}]
+      },
+      man2230 : {
+        title: "Vih == Sida?",
+        subTitle: "Hombre 22-30",
+        tagId: "chart-man-22-30-answer2",
+        data: [{}]
+      },
+      woman1517 : {
+        title: "Vih == Sida?",
+        subTitle: "Mujer 15-17",
+        tagId: "chart-woman-15-17-answer2",
+        data: [{}]
+      },
+      woman1821 : {
+        title: "Vih == Sida?",
+        subTitle: "Mujer 18-21",
+        tagId: "chart-woman-18-21-answer2",
+        data: [{}]
+      },
+      woman2230 : {
+        title: "Vih == Sida?",
+        subTitle: "Mujer 22-30",
+        tagId: "chart-woman-22-30-answer2",
+        data: [{}]
+      }
+    }
+  }
 
   private vihCollectionOptions = [
     {
       sourceCollection: '/vih-survey-filter/man/15-17',
       destinyCollection: '/vih-statistics/man15-17/answer2'
+    },
+    {
+      sourceCollection: '/vih-survey-filter/man/15-17',
+      destinyCollection: '/vih-statistics/man15-17/answer2'
+    },
+    {
+      sourceCollection: '/vih-survey-filter/man/18-21',
+      destinyCollection: '/vih-statistics/man18-21/answer2'
     },
     {
       sourceCollection: '/vih-survey-filter/man/18-21',
@@ -25,12 +79,28 @@ export class Answer2Component implements OnInit {
       destinyCollection: '/vih-statistics/man22-30/answer2'
     },
     {
+      sourceCollection: '/vih-survey-filter/man/22-30',
+      destinyCollection: '/vih-statistics/man22-30/answer2'
+    },
+    {
+      sourceCollection: '/vih-survey-filter/woman/15-17',
+      destinyCollection: '/vih-statistics/woman15-17/answer2'
+    },
+    {
       sourceCollection: '/vih-survey-filter/woman/15-17',
       destinyCollection: '/vih-statistics/woman15-17/answer2'
     },
     {
       sourceCollection: '/vih-survey-filter/woman/18-21',
       destinyCollection: '/vih-statistics/woman18-21/answer2'
+    },
+    {
+      sourceCollection: '/vih-survey-filter/woman/18-21',
+      destinyCollection: '/vih-statistics/woman18-21/answer2'
+    },
+    {
+      sourceCollection: '/vih-survey-filter/woman/22-30',
+      destinyCollection: '/vih-statistics/woman22-30/answer2'
     },
     {
       sourceCollection: '/vih-survey-filter/woman/22-30',
@@ -41,19 +111,67 @@ export class Answer2Component implements OnInit {
   private answer2Options = [
     {
       docId: 'positive',
-      position: 'answer2.positive'
+      position: 'answer2.value',
+      value: 'Si'
     },
     {
       docId: 'negative',
-      position: 'answer2.negative'
+      position: 'answer2.value',
+      value: 'No'
     }
   ];
 
-  constructor(private answer1Service: Answer2Service) { }
+  constructor(private answer2Service: Answer2Service) { }
 
   ngOnInit() {
+    // Update answer2 statistics
+    this.answer2Service.getAndUpdateVihStatisticAnswer2(this.answer2Options, this.vihCollectionOptions);
     // Fetch statistic for gender and years
-    this.answer1Service.fetchAnswer2Man1517Statistic();
+    this.answer2Service.fetchAnswer2Man1517Statistic();
+    this.answer2Service.fetchAnswer2Man1821Statistic();
+    this.answer2Service.fetchAnswer2Man2230Statistic();
+    this.answer2Service.fetchAnswer2Woman1517Statistic();
+    this.answer2Service.fetchAnswer2Woman1821Statistic();
+    this.answer2Service.fetchAnswer2Woman2230Statistic();
+
+    this.answer2Man1517StatisticsSubscription = this.answer2Service.answer2Man1517StatisticsChanged
+      .subscribe((availableAnswer2Statistics) => {
+        this.statistics.answer2.man1517.data = availableAnswer2Statistics;
+        //console.log('data refresh: ', this.statistics.answer2.man1517.data);
+      });
+    this.answer2Man1821StatisticsSubscription = this.answer2Service.answer2Man1821StatisticsChanged
+      .subscribe((availableAnswer1Statistics) => {
+        this.statistics.answer2.man1821.data = availableAnswer1Statistics;
+      });
+
+    this.answer2Man2230StatisticsSubscription = this.answer2Service.answer2Man2230StatisticsChanged
+      .subscribe((availableAnswer1Statistics) => {
+        this.statistics.answer2.man2230.data = availableAnswer1Statistics;
+      });
+
+    this.answer2Woman1517StatisticsSubscription = this.answer2Service.answer2Woman1517StatisticsChanged
+      .subscribe((availableAnswer1Statistics) => {
+        this.statistics.answer2.woman1517.data = availableAnswer1Statistics;
+      });
+
+    this.answer2Woman1821StatisticsSubscription = this.answer2Service.answer2Woman1821StatisticsChanged
+      .subscribe((availableAnswer1Statistics) => {
+        this.statistics.answer2.woman1821.data = availableAnswer1Statistics;
+      });
+    
+    this.answer2Woman2230StatisticsSubscription = this.answer2Service.answer2Woman2230StatisticsChanged
+      .subscribe((availableAnswer1Statistics) => {
+        this.statistics.answer2.woman2230.data = availableAnswer1Statistics;
+      });
+  }
+
+  ngOnDestroy() {
+    this.answer2Man1517StatisticsSubscription.unsubscribe();
+    this.answer2Man1821StatisticsSubscription.unsubscribe();
+    this.answer2Man2230StatisticsSubscription.unsubscribe();
+    this.answer2Woman1517StatisticsSubscription.unsubscribe();
+    this.answer2Woman1821StatisticsSubscription.unsubscribe();
+    this.answer2Woman2230StatisticsSubscription.unsubscribe();
   }
 
 }
--- src/app/statistics/answer2/answer2.service.ts
+++ src/app/statistics/answer2/answer2.service.ts
@@ -7,8 +7,13 @@ import { map } from 'rxjs/operators';
 @Injectable()
 export class Answer2Service {
 
-    answer2Man1517Statistics: PieData[];
+
     answer2Man1517StatisticsChanged = new Subject<PieData[]>();
+    answer2Man1821StatisticsChanged = new Subject<PieData[]>();
+    answer2Man2230StatisticsChanged = new Subject<PieData[]>();
+    answer2Woman1517StatisticsChanged = new Subject<PieData[]>();
+    answer2Woman1821StatisticsChanged = new Subject<PieData[]>();
+    answer2Woman2230StatisticsChanged = new Subject<PieData[]>();
 
 
     constructor(private db: AngularFirestore) {}
@@ -28,7 +33,96 @@ export class Answer2Service {
             }))
             .subscribe((answer2: PieData[]) => {
                 this.answer2Man1517StatisticsChanged.next([...answer2]);
-                console.log('fetch answer2: ', answer2);
+            });
+    }
+
+    // get Answer2 Man 18-21 Statistic
+    fetchAnswer2Man1821Statistic(): void {
+        this.db
+            .collection('/vih-statistics/man18-21/answer2')
+            .snapshotChanges()
+            .pipe(map((docArray) => {
+                return docArray.map((doc) => {
+                    return { 
+                        //id: doc.payload.doc.id,
+                        ...doc.payload.doc.data()
+                    };
+                });
+            }))
+            .subscribe((answer2: PieData[]) => {
+                this.answer2Man1821StatisticsChanged.next([...answer2]);
+            });
+    }
+
+    // get Answer2 Man 22-30 Statistic
+    fetchAnswer2Man2230Statistic(): void {
+        this.db
+            .collection('/vih-statistics/man22-30/answer2')
+            .snapshotChanges()
+            .pipe(map((docArray) => {
+                return docArray.map((doc) => {
+                    return { 
+                        //id: doc.payload.doc.id,
+                        ...doc.payload.doc.data()
+                    };
+                });
+            }))
+            .subscribe((answer2: PieData[]) => {
+                this.answer2Man2230StatisticsChanged.next([...answer2]);
+            });
+    }
+
+    // get Answer2 Woman 15-17 Statistic
+    fetchAnswer2Woman1517Statistic(): void {
+        this.db
+            .collection('/vih-statistics/woman15-17/answer2')
+            .snapshotChanges()
+            .pipe(map((docArray) => {
+                return docArray.map((doc) => {
+                    return { 
+                        //id: doc.payload.doc.id,
+                        ...doc.payload.doc.data()
+                    };
+                });
+            }))
+            .subscribe((answer2: PieData[]) => {
+                this.answer2Woman1517StatisticsChanged.next([...answer2]);
+            });
+    }
+
+    // get Answer2 Woman 18-21 Statistic
+    fetchAnswer2Woman1821Statistic(): void {
+        this.db
+            .collection('/vih-statistics/woman18-21/answer2')
+            .snapshotChanges()
+            .pipe(map((docArray) => {
+                return docArray.map((doc) => {
+                    return { 
+                        //id: doc.payload.doc.id,
+                        ...doc.payload.doc.data()
+                    };
+                });
+            }))
+            .subscribe((answer2: PieData[]) => {
+                this.answer2Woman1821StatisticsChanged.next([...answer2]);
+            });
+    }
+
+    // get Answer2 Woman 18-21 Statistic
+    fetchAnswer2Woman2230Statistic(): void {
+        this.db
+            .collection('/vih-statistics/woman22-30/answer2')
+            .snapshotChanges()
+            .pipe(map((docArray) => {
+                return docArray.map((doc) => {
+                    return { 
+                        //id: doc.payload.doc.id,
+                        ...doc.payload.doc.data()
+                    };
+                });
+            }))
+            .subscribe((answer2: PieData[]) => {
+                this.answer2Woman2230StatisticsChanged.next([...answer2]);
             });
     }
 
@@ -36,10 +130,10 @@ export class Answer2Service {
 
         for (let indexColecction = 0; indexColecction < vihCollectionOptions.length; indexColecction ++) {
           for (let index = 0; index < answer1Options.length; index ++) {
-            //console.log(index);
+            //console.log(index, ' - ', indexColecction, ' - ', vihCollectionOptions[indexColecction].sourceCollection, ' ', answer1Options[index].position, ' ', answer1Options[index].value);
             // get number of surveys
             this.db
-              .collection(vihCollectionOptions[indexColecction].sourceCollection, ref => ref.where(answer1Options[index].position,'==', true))
+              .collection(vihCollectionOptions[indexColecction].sourceCollection, ref => ref.where(answer1Options[index].position,'==', answer1Options[index].value))
               .valueChanges()
               .subscribe((surveys) => {
                 //console.log('get ',answer1Options[index].docId ,' ',surveys.length);
