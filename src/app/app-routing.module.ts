bf74d4ac1fd768fbccf9500e8821aac956961871
Ivan Vitali <webcoder.ivan@gmail.com>
2018-09-03 22:41:52 -0300

    se trabajo la proteccion de rutas, authGuard.ts, donde se definio el acceso a estadistica solo para usuarios logueados.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,19 @@ import { SignupComponent } from "./auth/signup/signup.component";
 import { LoginComponent } from "./auth/login/login.component";
 import { SurveyComponent } from "./survey/survey.component";
 import { StatisticsComponent } from "./statistics/statistics.component";
+import { AuthGuard } from "./auth/auth.guard";
 
 const routes: Routes = [
     { path: '', component: WelcomeComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'login', component: LoginComponent },
     { path: 'survey', component: SurveyComponent },
-    { path: 'statistics', component: StatisticsComponent }
+    { path: 'statistics', component: StatisticsComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [AuthGuard]
 })
 export class AppRoutingModule {}
